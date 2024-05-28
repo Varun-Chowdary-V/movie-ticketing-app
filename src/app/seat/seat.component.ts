a1bb75578995102f92bdaba7400c336414dbfad3
Varun-Chowdary-V <21bq1a05n9.vvit@gmail.com>
2024-05-28 13:03:31 +0530

    Initial commit

---
+++ src/app/seat/seat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat',
+  templateUrl: './seat.component.html',
+  styleUrl: './seat.component.css'
+})
+export class SeatComponent {
+
+}
